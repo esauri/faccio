6a56578817eaf8bb17d3b927a095bf25cf0151d9
Erick Sauri <ericksauri@hey.com>
2023-06-21 18:33:50 -0400

    Initial Commit

---
+++ faccio/faccioApp.swift
@@ -0,0 +1,17 @@
+//
+//  faccioApp.swift
+//  faccio
+//
+//  Created by Erick Sauri on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct faccioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
